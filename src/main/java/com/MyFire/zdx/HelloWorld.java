13cadc9e37d071d4ed7a5203a6bfcc99470c595e
BeiFengXiaoYue <3134506101@qq.com>
2026-09-07 14:39:16 +0800

    Initial commit

---
+++ src/main/java/com/MyFire/zdx/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.MyFire.zdx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("测试");
+        primaryStage.show();
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/MyFire/zdx/Main.java
@@ -0,0 +1,7 @@
+package com.MyFire.zdx;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
